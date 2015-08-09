e5b189130ffc5cae7050c0cd8b0e9766cef2bd59
gowithhao@yahoo.com <fuhao@fuhaodeMacBook-Pro.local>
2015-08-09 18:28:11 -0500

    start to translate

--- test.php
+++ test.php
@@ -1,56 +1,83 @@
 <html>
+<head>
+	<style type="text/css">
+		li {
+			cursor: pointer;
+			width: 10em;
+		}
+	</style>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+	<script>
+		function redirect(){
+			// document.getElementById('my_form').target = 'my_iframe'; //'my_iframe' is the name of the iframe
+			document.getElementById('my_form').submit();
+		}
+		function refresh(){
+			window.location.reload();
+		}
+		
+	</script>
+</head>
+
+
+
 <body bgcolor = 'white'>
 
 <h1 align = 'center'>1080 TEST PAGE</h1>
 <br />
+<input type="button" name="action" value="refresh" onclick="refresh()"/>
+
 <?php
 $dir = "uploads";
-
-
-
 function listTxtFiles($dir) {
-	// directory we want to scan
-	$dircontents = scandir($dir);
-	// list the contents
-	echo '<ul id = "listFile">';
-	foreach ($dircontents as $file) {
-		$extension = pathinfo($file, PATHINFO_EXTENSION);
-		if ($extension == 'txt') {
-			echo "<li onclick = \"clickFunc(this)\" class = \"listFile\">$file </li>";
-		}
-	}
-	echo '</ul>';
-}
-listTxtFiles($dir);
+			// directory we want to scan
+			$dircontents = scandir($dir);
+			// list the contents
+			echo '<div>';
+			echo '<ul id = "listFile">';
+			foreach ($dircontents as $file) {
+				$extension = pathinfo($file, PATHINFO_EXTENSION);
+				if ($extension == 'txt') {
+					$file = $file;
+					echo "<li onclick = \"clickFunc(this)\" class = \"listFile\">$file </li>";
+				}
+			}
+			echo '</ul>';
+			echo '</div >';
 
+		}
+listTxtFiles($dir);
 ?>
 
-<form action = "upload.php" method = "post" enctype = "multipart/form-data">
-	<input type = "file" name = "myfile">
-	<input type = "submit" name = "upload">
+<form id="my_form" name="form" action="upload.php" method="POST" enctype="multipart/form-data" >
+<div id="upload_section">
+    <input name="file" type="file" />
+    <input type="button" name="action" value="Upload" onclick="redirect()"/>
+    <br />
+<!--     <iframe id='my_iframe' name='my_iframe' src="" width="200" height="200">
+	</iframe> -->
+</div>
 </form>
 
-<!-- comment-->
+
 <a href="http://www.google.com"><img src = "g.png" width = '100' height '30'/>
 </a>
-<br />
-<input id ='num1'> + <input id = 'num2'>
-<script src = "main.js">
-</script>
+
 <script>
 function li_clickable(){
- 	var myLi = document.getElementById('listFile').getElementsByTagName('li');
+		 	var myLi = document.getElementById('listFile').getElementsByTagName('li');
 
-    var calls = function() {
-    	//alert(this.innerHTML)
-    	window.location.href = "display.php?name=" + "uploads/" +this.innerHTML; 
-    };
-    
-    for (i = 0; i < myLi.length; i++) {
-        myLi[i].addEventListener('click', calls, false);
-    }
-}
+		    var calls = function() {
+		    	//alert(this.innerHTML)
+		    	window.location.href = "display.php?name=" + "uploads/" +this.innerHTML; 
+		    };
+		    
+		    for (i = 0; i < myLi.length; i++) {
+		        myLi[i].addEventListener('click', calls, false);
+		    }
+		}
 li_clickable()
 </script>
+
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -1,15 +1,11 @@
 <?php
-function display_file($target_file){
-	$myfile = fopen($target_file, "r") or die("Unable to open file!");
-	echo fread($myfile,filesize($target_file));
-}
 
 if (!file_exists('uploads'))
 {
     mkdir("uploads", 0777, true);
 }
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["myfile"]["name"]);
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
@@ -20,10 +16,18 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)) {
-    	echo "succeed";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+    	header("Location:test.php");
+    	exit;
+    }
+    else{
+   		echo "<script>alert('upload failed');</script>";
+   		echo "<script>window.location = 'test.php' </script>";
+  //  		while (ob_get_status()) {
+		//     ob_end_clean();
+		// }
+  //  		header("Location:test.php");
+  //   	exit;
     }
 }
 ?>
